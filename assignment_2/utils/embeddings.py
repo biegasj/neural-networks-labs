db2f52660fcd0ff6e2f26600ddf3cf59b9e70141
biegasj <biegasj@gmail.com>
2023-11-14 15:17:53 +0100

    Correct the process of saving data for dimensionality reduction

--- assignment_2/utils/embeddings.py
+++
@@ -1,6 +0,0 @@
-import pandas as pd
-
-
-def save_embeddings_to_tsv(embeddings: any, path: str) -> None:
-    df = pd.DataFrame(data=embeddings)
-    df.to_csv(path, sep="\t", header=False, index=False)
